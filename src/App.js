c09eb366bf7021f2237312bd5fdc81a00938499d
Piligrim41m <lightsun41m@gmail.com>
2023-01-10 08:54:03 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 
 import s from './App.module.css';
@@ -26,6 +27,7 @@ function App() {
     }
     fetchGallary();
     
+  
   }, [searchQuery]);
 
   const fetchGallary = () => {
